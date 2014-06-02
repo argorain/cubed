3cb3354df006a44075ffd3f43152514673e12972
vojtech.vladyka <vojtech.vladyka@gmail.com>
2014-06-02 10:26:35 +0200

    Now he is facing right side

--- Cubed/src/cubed/Man.java
+++ Cubed/src/cubed/Man.java
@@ -5,132 +5,137 @@ import java.util.TimerTask;
 import srengine.Entity;
 import srengine.utils.Serialiser;
 
-public class Man extends Entity{
+public class Man extends Entity {
 
-	Balloon balloon  = null;
-	String name;
-	String race;
-	String age;
-	String health="100%";
-	private static final int kX = 30;
-	private static final int kY = -85;
-	int relX, relY, trelX, trelY, mrelX, mrelY;
-	boolean moving = false;
-        int step=0;
-        int dx=0,dy=0,rdx=0,rdy=0;
-        final int dv=3;
-        
+    Balloon balloon = null;
+    String name;
+    String race;
+    String age;
+    String health = "100%";
+    private static final int kX = 30;
+    private static final int kY = -85;
+    int relX, relY, trelX, trelY, mrelX, mrelY;
+    boolean moving = false;
+    int step = 0;
+    int dx = 0, dy = 0, rdx = 0, rdy = 0;
+    final int dv = 3;
     private int BLOCKSIZE;
-	
-	public Man(float x, float y, String[] images, Serialiser s) {
-		super(x, y, images, s);
-	}
-	
-	public void setInfo(String name, String race, String age){
-		this.name = name;
-		this.age = age;
-		this.race= race;
-	}
-	
-	public void setBallonMessage(String message){
-		balloon.setText(message);
-	}
-	
-        @Override
-	public void setAngle(float angle){
+    private boolean inverted=false;
+
+    public Man(float x, float y, String[] images, Serialiser s) {
+        super(x, y, images, s);
+    }
+
+    public void setInfo(String name, String race, String age) {
+        this.name = name;
+        this.age = age;
+        this.race = race;
+    }
+
+    public void setBallonMessage(String message) {
+        balloon.setText(message);
+    }
+
+    @Override
+    public void setAngle(float angle) {
         super.setAngle(angle);
-        if(balloon != null)
-        	balloon.setAngle(angle);
+        if (balloon != null) {
+            balloon.setAngle(angle);
+        }
     }
 
-        @Override
-	public void setX(float x) {
+    @Override
+    public void setX(float x) {
         this.x = x;
-        if(balloon != null)
-        	balloon.setX(x+kX);
+        if (balloon != null) {
+            balloon.setX(x + kX);
+        }
     }
 
-        @Override
+    @Override
     public void setY(float y) {
         this.y = y;
-        if(balloon != null)
-        	balloon.setY(y+kY);
+        if (balloon != null) {
+            balloon.setY(y + kY);
+        }
     }
-    
-    public void setBalloon(Balloon balloon){
-    	this.balloon = balloon;
-    	balloon.setX(x+kX);
-    	balloon.setY(y+kY);
+
+    public void setBalloon(Balloon balloon) {
+        this.balloon = balloon;
+        balloon.setX(x + kX);
+        balloon.setY(y + kY);
     }
-    
-    public void setRelPos(int x, int y){
-    	relX= x;
-    	relY= y;
-        setX(x*BLOCKSIZE);
-        setY(y*BLOCKSIZE);
+
+    public void setRelPos(int x, int y) {
+        relX = x;
+        relY = y;
+        setX(x * BLOCKSIZE);
+        setY(y * BLOCKSIZE);
     }
-    
-    public int relX(){
-    	return relX;
+
+    public int relX() {
+        return relX;
     }
-    
-    public int relY(){
-    	return relY;
+
+    public int relY() {
+        return relY;
     }
-    
-    public void move(){
-    	moving = true;
+
+    public void move() {
+        moving = true;
     }
-    
-    public boolean isMoving(){
-    	return moving;
+
+    public boolean isMoving() {
+        return moving;
     }
 
     void setTargetRelPos(int px, int py) {
-        trelX=px;
-        trelY=py;
+        trelX = px;
+        trelY = py;
     }
-    
-    boolean onSpot(){
-        return (relX==trelX&&relY==trelY);
+
+    boolean onSpot() {
+        return (relX == trelX && relY == trelY);
     }
 
     void moveTo() {
         this.run();
         Timer run = new Timer();
-        moving=true;
-        step=0;
-        
-        dx=dy=rdx=rdy=0;
-        
-        if(mrelX*BLOCKSIZE>getX()){
-            dx=dv;
+        moving = true;
+        step = 0;
+
+        dx = dy = rdx = rdy = 0;
+
+        if (mrelX * BLOCKSIZE > getX()) {
+            dx = dv;
             rdx++;
-        }else if(mrelX*BLOCKSIZE<getX()){
-            dx=-dv;
+            lookRight();
+        } else if (mrelX * BLOCKSIZE < getX()) {
+            dx = -dv;
             rdx--;
+            lookLeft();
         }
-        if(mrelY*BLOCKSIZE>getY()){
-            dy=dv;
+        if (mrelY * BLOCKSIZE > getY()) {
+            dy = dv;
             rdy++;
-        }else if(mrelY*BLOCKSIZE<getY()){
-            dy=-dv;
+        } else if (mrelY * BLOCKSIZE < getY()) {
+            dy = -dv;
             rdy--;
         }
         run.scheduleAtFixedRate(new TimerTask() {
-            
             @Override
             public void run() {
-                               
-                setX(getX()+dx);
-                setY(getY()+dy);
-                if(step%2==0)
+
+                setX(getX() + dx);
+                setY(getY() + dy);
+                if (step % 2 == 0) {
                     redraw();
-                if(step<(int)(BLOCKSIZE/dv))
+                }
+                if (step < (int) (BLOCKSIZE / dv)) {
                     step++;
-                else{
-                    moving=false;
-                    setRelPos(relX+rdx, relY+rdy);
+                } else {
+                    moving = false;
+                    setRelPos(relX + rdx, relY + rdy);
                     super.cancel();
                 }
             }
@@ -138,11 +143,25 @@ public class Man extends Entity{
     }
 
     void setRelPosToMove(int ktx, int kty) {
-        mrelX=ktx;
-        mrelY=kty;
+        mrelX = ktx;
+        mrelY = kty;
     }
 
     void setBlocksize(int BLOCKSIZE) {
-        this.BLOCKSIZE=BLOCKSIZE;
+        this.BLOCKSIZE = BLOCKSIZE;
+    }
+    
+    void lookLeft(){
+        if(!inverted){
+            invertOverY();
+            inverted=true;
+        }
+    }
+    
+    void lookRight(){
+        if(inverted){
+            invertOverY();
+            inverted=false;
+        }
     }
 }
